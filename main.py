12d3ca42ea58be43f3107ad5efdf09fa305b07f4
Palbha Kulkarni (Nazwale) <palbhanazwale@gmail.com>
2024-09-05 23:42:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ input=st.text_input("Input ",key="input")
 submit=st.button("Ask the question")
 
 if submit:
-  response=get_gemini_rsponse(input)
+  response=get_gemini_response(input)
   st.subheader("The response is ")
   st.write(response)
   
