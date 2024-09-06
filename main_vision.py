7b9603dbfa3ce68b16b1d77e7c11ee037d34615a
Palbha Kulkarni (Nazwale) <palbhanazwale@gmail.com>
2024-09-05 23:47:50 -0400

    Update main_vision.py

--- main_vision.py
+++ main_vision.py
@@ -1,13 +1,13 @@
 from dotenv import load_dotenv
 load_dotenv() # loading env variables
-
+from PIL import Image
 import streamlit as st
 import os
 import google.generativeai as genai
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-model=genai.GenerativeModel("gemini-pro-vision")
+model=genai.GenerativeModel("gemini-1.5-flash")
 def get_gemini_response(input,image):
   if input!="":
     
@@ -27,13 +27,13 @@ uploaded_file=st.file_uploader("choose an image",type=["jpg","jpeg","pnng"])
 image=""
 
 if uploaded_file is not None:
-  image=Image.open(uplaoded_file)
-  st.image(image,caption="uploaded image",use_column_width-=True)
+  image=Image.open(uploaded_file)
+  st.image(image,caption="uploaded image",use_column_width=True)
                    
 submit=st.button("Tell me about the image")
 
 if submit:
-  response=get_gemini_rsponse(input,image)
+  response=get_gemini_response(input,image)
   st.subheader("The response is ")
   st.write(response)
   
