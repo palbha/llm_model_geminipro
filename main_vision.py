9e81390c7d78a17784621b118c01534da89bf800
Palbha Kulkarni (Nazwale) <palbhanazwale@gmail.com>
2024-09-05 20:28:45 -0400

    Create main_vision.py

---
+++ main_vision.py
@@ -0,0 +1,41 @@
+from dotenv import load_dotenv
+load_dotenv() # loading env variables
+
+import streamlit as st
+import os
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+model=genai.GenerativeModel("gemini-pro-vision")
+def get_gemini_response(input,image):
+  if input!="":
+    
+    response=model.generate_content([input,image])
+  else:
+    response=model.generate_content(image)
+    
+  return response.text
+
+
+st.set_page_config(page_title=" Gemini Image Demo")
+st.header("Gemini  Application")
+
+input=st.text_input("Input Pormpt ",key="input")
+
+uploaded_file=st.file_uploader("choose an image",type=["jpg","jpeg","pnng"])
+image=""
+
+if uploaded_file is not None:
+  image=Image.open(uplaoded_file)
+  st.image(image,caption="uploaded image",use_column_width-=True)
+                   
+submit=st.button("Tell me about the image")
+
+if submit:
+  response=get_gemini_rsponse(input,image)
+  st.subheader("The response is ")
+  st.write(response)
+  
+  
+  
